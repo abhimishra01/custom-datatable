7a9fde445e1c9e6aa613012dd7f3338003b7e348
Abhishek Mishra <abhisk.mhra@gmail.com>
2023-07-16 23:21:55 +0530

    chore: centralize `TableHead` `Header` prop types for reusability

--- src/components/DataTable/types.ts
+++ src/components/DataTable/types.ts
@@ -1,12 +1,9 @@
+import { Header } from "@/utils/types";
+
 export default interface DataTableType {
   row?: [];
   headers: Array<Header>;
   caption?: string;
   sortable?: boolean;
   pagination?: boolean;
-}
-export interface Header {
-  id: string;
-  label: string;
-  isNumeric?: boolean;
 }
